4176e04b14354d706d20667884b5070401b71dd6
jfnewsom <40766396+jfnewsom@users.noreply.github.com>
2026-03-26 12:20:46 -0500

    lab 4.3

--- components.py
+++ components.py
@@ -188,8 +188,8 @@ def topper_with_badge(badge_word: str, badge_num: str,
         <td style="vertical-align: bottom; padding: 0 0 0 16px;">
           <div class="lc-topper-title">{title}</div>
         </td>
-        <td style="width: 1%; white-space: nowrap; vertical-align: bottom; padding: 0 0 0 16px; text-align: right;">
-          <img src="../../branding/BatCity-logo-3D.png" alt="Bat City Collective" style="height: 100px; width: auto; display: block;">
+        <td style="width: 1%; white-space: nowrap; vertical-align: bottom; padding: 0 0 0 16px; text-align: right; height: 100%;">
+          <img src="../../branding/BatCity-logo-3D.png" alt="Bat City Collective" style="height: 100%; width: auto; display: block;">
         </td>
       </tr>{sub_row}
     </table>
